a909186cc2ececb0252ca1b9a6bfa24b6d0d87fb
Efe Ariaroo <efeariaroo@gmail.com>
2022-11-26 15:04:59 +0000

    Add finish puzzle

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,8 +2,10 @@ import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async ({ cookies }) => {
   const username = cookies.get("username");
+  const userToken = cookies.get("userToken");
   return {
     username,
+    userToken,
     // 'username': localStorage.getItem('username')
   };
 };
